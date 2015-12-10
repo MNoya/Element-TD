fa72ef9e9beeb4e7f5ed00864c72ef5ef3bb84c2
ynohtna92 <a_n_t_h_o_n_y8@hotmail.com>
2015-12-10 22:40:35 +1100

    Remove colons and fix bounty for swarm creep

--- game/dota_addons/element_td/scripts/vscripts/creeps/swarm.lua
+++ game/dota_addons/element_td/scripts/vscripts/creeps/swarm.lua
@@ -11,50 +11,60 @@ CreepSwarm = createClass({
 	{
 		className = "CreepSwarm"
 	},
-CreepBasic);
+CreepBasic)
 
 function CreepSwarm:OnTakeDamage(keys)
 	print("CreepSwarm:OnHit")
 	if self.creep:GetHealth() > 0 and self.creep:GetHealthPercent() <= 50 and not self.creep.isSwarm then
 		print("Creating Swarm")
-		self.creep.isSwarm = true;
+		self.creep.isSwarm = true
 
-		local swarm = SpawnEntity(self.creep:GetUnitName(), self.creep.playerID, self.creep:GetOrigin());
-		swarm.class = creepClass;
-		swarm.playerID = self.creep.playerID;
-		swarm.waveObject = self.creep.waveObject;
-		self.creep.waveObject:RegisterCreep(swarm:entindex());
+		local swarm = SpawnEntity(self.creep:GetUnitName(), self.creep.playerID, self.creep:GetOrigin())
+		swarm.class = creepClass
+		swarm.playerID = self.creep.playerID
+		swarm.waveObject = self.creep.waveObject
+		self.creep.waveObject:RegisterCreep(swarm:entindex())
 		self.creep.waveObject.creepsRemaining = self.creep.waveObject.creepsRemaining + 1 -- Increment creep count
-		swarm.isSwarm = true;
-		local newMaxHealth = self.creep:GetMaxHealth()/2;
-		swarm:SetMaxHealth(newMaxHealth);
-		swarm:SetBaseMaxHealth(newMaxHealth);
-		swarm:SetHealth(newMaxHealth);
-		swarm:SetDeathXP(0);
-		swarm:SetForwardVector(self.creep:GetForwardVector());
+		swarm.isSwarm = true
+		local newMaxHealth = self.creep:GetMaxHealth()/2
+		swarm:SetMaxHealth(newMaxHealth)
+		swarm:SetBaseMaxHealth(newMaxHealth)
+		swarm:SetHealth(newMaxHealth)
+		swarm:SetDeathXP(0)
+		swarm:SetForwardVector(self.creep:GetForwardVector())
 
-		self.creep:SetMaxHealth(newMaxHealth);
-		self.creep:SetBaseMaxHealth(newMaxHealth);
-		self.creep:SetHealth(newMaxHealth);
+		self.creep:SetMaxHealth(newMaxHealth)
+		self.creep:SetBaseMaxHealth(newMaxHealth)
+		self.creep:SetHealth(newMaxHealth)
 
-		local newScale = self.creep:GetModelScale()*0.8;
+		local newScale = self.creep:GetModelScale()*0.8
 
-		self.creep:SetModelScale(newScale);
-		swarm:SetModelScale(newScale);
+		self.creep:SetModelScale(newScale)
+		swarm:SetModelScale(newScale)
 
-		local playerID = self.creep.playerID;
-		local wave = self.creep.waveObject:GetWaveNumber();
-		local bounty = math.ceil(GetPlayerDifficulty(playerID):GetBountyForWave(wave)/2);
+		local playerID = self.creep.playerID
+		local wave = self.creep.waveObject:GetWaveNumber()
 
-		self.creep:SetMaximumGoldBounty(bounty);
-		self.creep:SetMinimumGoldBounty(bounty);
+		-- split the bounty between the two units even if it is an odd amount
+		local bounty1 = GetPlayerDifficulty(playerID):GetBountyForWave(wave)
+		local bounty2
+		if bounty1 % 2 == 0 then
+			bounty1 = bounty1 / 2
+			bounty2 = bounty1
+		else
+			bounty1 = math.floor(bounty1 / 2)
+			bounty2 = bounty1 + 1
+		end
 
-		swarm:SetMaximumGoldBounty(bounty);
-		swarm:SetMinimumGoldBounty(bounty);
+		self.creep:SetMaximumGoldBounty(bounty1)
+		self.creep:SetMinimumGoldBounty(bounty1)
 
-		local playerData = GetPlayerData(playerID);
-		CreateMoveTimerForCreep(swarm, playerData.sector + 1);
+		swarm:SetMaximumGoldBounty(bounty2)
+		swarm:SetMinimumGoldBounty(bounty2)
+
+		local playerData = GetPlayerData(playerID)
+		CreateMoveTimerForCreep(swarm, playerData.sector + 1)
 	end
 end
 
-RegisterCreepClass(CreepSwarm, CreepSwarm.className);
+RegisterCreepClass(CreepSwarm, CreepSwarm.className)
