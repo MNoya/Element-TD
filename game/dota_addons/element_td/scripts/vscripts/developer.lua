cac51f2a37e77bb1ae31c2bc86e26b6c5da3958e
MNoya <martinnoya@gmail.com>
2015-12-15 16:28:08 -0300

    AttachWearables

--- game/dota_addons/element_td/scripts/vscripts/developer.lua
+++ game/dota_addons/element_td/scripts/vscripts/developer.lua
@@ -140,72 +140,98 @@ function ElementTD:Dev(playerID)
 end
 
 function ElementTD:MakeSets()
-    if not GameRules.modelmap then
-        GameRules.items = LoadKeyValues("scripts/items/items_game.txt")['items']
-        GameRules.modelmap = {}
-        for k,v in pairs(GameRules.items) do
-            if v.name and v.prefab ~= "loading_screen" then
-                GameRules.modelmap[v.name] = k
+    if not GameRules.modelmap then MapWearables() end
+
+    -- Generate all sets for each hero
+    local heroes = LoadKeyValues("scripts/npc/npc_heroes.txt")
+    local filePath = "../../dota_addons/element_td/scripts/AttachWearables.txt"
+    local file = io.open(filePath, 'w')
+
+    for k,v in pairs(heroes) do
+        GenerateAllSetsForHero(file, k)
+    end
+
+    file:close()    
+    
+end
+
+function GenerateAllSetsForHero( file, heroName )
+    local indent = "    "
+    file:write(indent..string.rep("/", 60).."\n")
+    file:write(indent.."// Cosmetic Sets for "..heroName.."\n")
+    file:write(indent..string.rep("/", 60).."\n")
+    GenerateDefaultBlock(file, heroName)
+
+    -- Find sets that match this hero
+    for key,values in pairs(GameRules.items) do
+        if values.name and values.used_by_heroes and values.prefab and values.prefab == "bundle" then
+            if type(values.used_by_heroes) == "table" then
+                for k,v in pairs(values.used_by_heroes) do
+                    if k == heroName then
+                        GenerateBundleBlock(file, values.name)
+                    end
+                end
             end
         end
     end
+    file:write("\n")
+end
 
-    -- Default hero set to generate
-    GenerateDefaultBlock("npc_dota_hero_earth_spirit")
-
-    -- Sets to generate
-    local setNames = {}
-
-    local sets = {}
-    for k,v in pairs(setNames) do
-        sets[v] = v
-    end
-
-    for code,values in pairs(GameRules.items) do
-        if values.name and values.bundle and values.prefab and values.prefab == "bundle" and sets[values.name] and sets[values.name] == values.name then
-            GenerateAttachWearablesBlock(values.name, values.bundle)   
+function MapWearables()
+    GameRules.items = LoadKeyValues("scripts/items/items_game.txt")['items']
+    GameRules.modelmap = {}
+    for k,v in pairs(GameRules.items) do
+        if v.name and v.prefab ~= "loading_screen" then
+            GameRules.modelmap[v.name] = k
         end
     end
 end
 
-function GenerateDefaultBlock( heroName )
-    print("    \"Creature\"")
-    print("    {")
-    print("        \"AttachWearables\" ".."// Default "..heroName)
-    print("        {")
+function GenerateDefaultBlock( file, heroName )
+    file:write("    \"Creature\"\n")
+    file:write("    {\n")
+    file:write("        \"AttachWearables\" ".."// Default "..heroName.."\n")
+    file:write("        {\n")
     local defCount = 1
     for code,values in pairs(GameRules.items) do
         if values.name and values.prefab == "default_item" and values.used_by_heroes then
             for k,v in pairs(values.used_by_heroes) do
                 if k == heroName then
                     local itemID = GameRules.modelmap[values.name]
-                    GenerateItemDefLine( defCount, itemID, values.name )
+                    GenerateItemDefLine( file, defCount, itemID, values.name )
                     defCount = defCount + 1
                 end
             end
         end
     end
-    print("        }")
-    print("    }")
+    file:write("        }\n")
+    file:write("    }\n")
 end
+ 
+function GenerateBundleBlock( file, setname )
+    local bundle = {}
+    for code,values in pairs(GameRules.items) do
+        if values.name and values.name == setname and values.prefab and values.prefab == "bundle" then
+            bundle = values.bundle
+        end
+    end
 
-function GenerateAttachWearablesBlock(setname, bundle)
-    print("    \"Creature\"")
-    print("    {")
-    print("        \"AttachWearables\" ".."// "..setname)
-    print("        {")
+    file:write("    \"Creature\"\n")
+    file:write("    {\n")
+    file:write("        \"AttachWearables\" ".."// "..setname.."\n")
+    file:write("        {\n")
     local wearableCount = 1
     for k,v in pairs(bundle) do
         local itemID = GameRules.modelmap[k]
         if itemID then
-            GenerateItemDefLine(wearableCount, itemID, k)
+            GenerateItemDefLine(file, wearableCount, itemID, k)
             wearableCount = wearableCount+1
         end
     end
-    print("        }")
-    print("    }")
+    file:write("        }\n")
+    file:write("    }\n")
 end
-
-function GenerateItemDefLine( i, itemID, comment )
-    print("            \""..tostring(i).."\" { ".."\"ItemDef\" \""..itemID.."\" } // "..comment)
+ 
+function GenerateItemDefLine( file, i, itemID, comment )
+    file:write("            \""..tostring(i).."\" { ".."\"ItemDef\" \""..itemID.."\" } // "..comment.."\n")
 end
