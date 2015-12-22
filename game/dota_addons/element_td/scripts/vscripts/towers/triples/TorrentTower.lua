2fa42398fd01eb4deeb2bfdb81d70e7e4983b7c6
ynohtna92 <a_n_t_h_o_n_y8@hotmail.com>
2015-12-23 10:56:58 +1100

    Tidal Tower updated to spec.

--- game/dota_addons/element_td/scripts/vscripts/towers/triples/TorrentTower.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/triples/TorrentTower.lua
@@ -17,58 +17,45 @@ TorrentTower = createClass({
     },
 nil)
 
-function TorrentTower:OnAttackStart(keys)
-    self.attacks = self.attacks + 1
-    if self.attacks == 10 then
-        self.attacks = 0
-        if self.ability:GetLevel() < 10 then
-            self.ability:SetLevel(self.ability:GetLevel() + 1)
-            self.tower:SetModifierStackCount("modifier_douse_passive", self.ability, self.ability:GetLevel())
-        end
+function TorrentTower:ResetDamage(keys)
+    if self.resetTimer then
+        Timers:RemoveTimer(self.resetTimer)
     end
-    if self.ability:GetLevel() == 10 and self.ability:IsFullyCastable() and self.ability:GetAutoCastState() then
-        self.ability:StartCooldown(5)
-        self:OnDouseCast()
-    end
-end
-
-function TorrentTower:OnDouseCast(keys)
-    local creeps = GetCreepsInArea(self.tower:GetOrigin(), self.aoe)
-    for _,creep in pairs(creeps) do
-        if creep:IsAlive() then
-            local player = PlayerResource:GetPlayer(self.tower:GetOwner():GetPlayerID())
-            local particle = ParticleManager:CreateParticleForPlayer("particles/units/heroes/hero_kunkka/kunkka_spell_torrent_splash.vpcf", PATTACH_ABSORIGIN, self.tower, player)
-            ParticleManager:SetParticleControl(particle, 0, GetGroundPosition(creep:GetAbsOrigin(), nil))
-            ParticleManager:ReleaseParticleIndex(particle)
-            DamageEntity(creep, self.tower, self.douseDamage[self.ability:GetLevel()])
-        end
-    end
-    self.ability:SetLevel(0)
-    self.tower:SetModifierStackCount("modifier_douse_passive", self.ability, 0)
+    self.tidalStacks = 0
+    self.tower:RemoveModifierByName("modifier_tidal_splash_decay")
 end
 
 function TorrentTower:OnAttackLanded(keys)
     local target = keys.target
-    local damage = ApplyAttackDamageFromModifiers(self.tower:GetBaseDamageMax(), self.tower)
-    DamageEntity(target, self.tower, damage)
+    local damage = ApplyAttackDamageFromModifiers(self.tower:GetAverageTrueAttackDamage(), self.tower)
+
+    DamageEntitiesInArea(target:GetOrigin(), self.halfAOE - (self.tidalStacks * self.aoeDecay), self.tower, damage / 2)
+    DamageEntitiesInArea(target:GetOrigin(), self.fullAOE - (self.tidalStacks * (self.aoeDecay/2)), self.tower, damage / 2)
+
+    self.ability:ApplyDataDrivenModifier(self.tower, self.tower, "modifier_tidal_splash_decay", {})
+
+    if self.tidalStacks < 20 then
+        self.tidalStacks = self.tidalStacks + 1
+        self.tower:SetModifierStackCount("modifier_tidal_splash_decay", self.ability, self.tidalStacks)
+    end
+
+    if self.resetTimer ~= nil then
+        Timers:RemoveTimer(self.resetTimer)
+    end
+    self.resetTimer = Timers:CreateTimer(self.resetTime, function()
+        self:ResetDamage({})
+    end)
 end
 
 function TorrentTower:OnCreated()
-    self.ability = AddAbility(self.tower, "torrent_tower_douse_" .. self.tower:GetLevel())
-    self.ability:SetLevel(0)
-
-    Timers:CreateTimer(1, function()
-        if IsValidEntity(self.tower) then
-            self.tower:SetModifierStackCount("modifier_douse_passive", self.ability, 0)
-            return nil
-        end
-    end)
-
-    self.ability:ToggleAutoCast()
-
-    self.aoe = GetAbilitySpecialValue("torrent_tower_douse_" .. self.tower:GetLevel(), "aoe")
-    self.douseDamage = GetAbilitySpecialValue("torrent_tower_douse_" .. self.tower:GetLevel(), "damage")
-    self.attacks = 0
+    self.ability = AddAbility(self.tower, "tidal_tower_splash_decay", self.tower:GetLevel())
+    self.aoeDecay = GetAbilitySpecialValue("tidal_tower_splash_decay", "aoe")
+    self.fullAOE = tonumber(GetUnitKeyValue(self.towerClass, "AOE_Full"))
+    self.halfAOE = tonumber(GetUnitKeyValue(self.towerClass, "AOE_Half"))
+    self.douseDamage = GetAbilitySpecialValue("tidal_tower_splash_decay", "damage")
+    self.resetTime = GetAbilitySpecialValue("tidal_tower_splash_decay", "reset_time")
+    self.tidalStacks = 0
+    self.resetTimer = nil
 end
 
 RegisterTowerClass(TorrentTower, TorrentTower.className)
