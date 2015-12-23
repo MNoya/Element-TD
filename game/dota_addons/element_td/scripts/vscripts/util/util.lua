47b23f0eb56762cbe2146fd4a59370e52f62a80d
ynohtna92 <a_n_t_h_o_n_y8@hotmail.com>
2015-12-23 20:02:17 +1100

    Splash damage to elementals as well.

--- game/dota_addons/element_td/scripts/vscripts/util/util.lua
+++ game/dota_addons/element_td/scripts/vscripts/util/util.lua
@@ -152,7 +152,7 @@ end
 
 function GetCreepsInArea(center, radius)
     local creeps = FindUnitsInRadius(0, center, nil, radius, 
-                        DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_INVULNERABLE, FIND_ANY_ORDER, false)
+                        DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_INVULNERABLE, FIND_ANY_ORDER, false)
     for k, v in pairs(creeps) do
         if v:GetTeam() ~= DOTA_TEAM_NEUTRALS then
             creeps[k] = nil;
