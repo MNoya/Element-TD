321e25b80ef5aaca9e2995f2cc9a4a45371a9b09
ynohtna92 <a_n_t_h_o_n_y8@hotmail.com>
2016-01-31 11:04:30 +1100

    Fixes voting ui error.

--- game/dota_addons/element_td/scripts/vscripts/voting.lua
+++ game/dota_addons/element_td/scripts/vscripts/voting.lua
@@ -181,7 +181,7 @@ function ElementTD:OnPlayerVoted( table )
 		AddVote(VOTE_RESULTS.order, table.data.orderVote)
 		AddVote(VOTE_RESULTS.length, table.data.lengthVote)
 
-		local data = {playerID = playerID, gamemode = table.data.gamemodeVote, difficulty = table.data.difficultyVote, elements = table.data.elementsVote, endless = table.data.endless, order = table.data.orderVote, length = table.data.lengthVote}
+		local data = {playerID = playerID, gamemode = table.data.gamemodeVote, difficulty = table.data.difficultyVote, elements = table.data.elementsVote, endless = table.data.endlessVote, order = table.data.orderVote, length = table.data.lengthVote}
 		CustomGameEventManager:Send_ServerToAllClients( "etd_vote_display", data )
 
 		--check to see if all players have finished voting
