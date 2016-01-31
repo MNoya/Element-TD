e679f5a219b4b8932b7adf50f8b277013a89218b
MNoya <martinnoya@gmail.com>
2016-01-31 16:54:49 -0300

    Haste tower is now BAT based (buff)

--- game/dota_addons/element_td/scripts/vscripts/towers/triples/HasteTower.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/triples/HasteTower.lua
@@ -18,6 +18,7 @@ nil)
 function HasteTower:ResetAttackSpeed()
     self.tower:RemoveModifierByName("modifier_wrath")
     self.wrathStacks = 0
+    self.tower:SetBaseAttackTime(self.startingBAT)
 end
 
 function HasteTower:OnAttack(keys)
@@ -27,6 +28,7 @@ function HasteTower:OnAttack(keys)
 
     self.ability:ApplyDataDrivenModifier(self.tower, self.tower, "modifier_wrath", {})
     self.tower:SetModifierStackCount("modifier_wrath", self.ability, self.wrathStacks)
+    self.tower:SetBaseAttackTime(self.startingBAT - self.batStack * self.wrathStacks)
 
     if self.timer then
         Timers:RemoveTimer(self.timer)
@@ -46,8 +48,10 @@ function HasteTower:OnCreated()
     self.ability = AddAbility(self.tower, "haste_tower_wrath")
     self.wrathCap = GetAbilitySpecialValue("haste_tower_wrath", "cap")
     self.resetTime = GetAbilitySpecialValue("haste_tower_wrath", "reset_time")
+    self.batStack = GetAbilitySpecialValue("haste_tower_wrath", "bat_decrease_stack")
     self.wrathStacks = 0
     self.timer = nil
+    self.startingBAT = self.tower:GetBaseAttackTime()
 end
 
 RegisterTowerClass(HasteTower, HasteTower.className)
