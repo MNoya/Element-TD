0b82aa049a616ed61b4de78414a34ddff78f1737
MNoya <martinnoya@gmail.com>
2016-02-03 13:46:06 -0300

    Disabled sounds on autocast skills
    - Too annoying/spammy

--- game/dota_addons/element_td/scripts/vscripts/towers/duals/blacksmith/fire_up.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/duals/blacksmith/fire_up.lua
@@ -4,7 +4,7 @@ LinkLuaModifier("modifier_fire_up", "towers/duals/blacksmith/modifier_fire_up", 
 function blacksmith_tower_fire_up:OnSpellStart()
 	local caster = self:GetCaster()
 	local target = self:GetCursorTarget()
-	Sounds:EmitSoundOnClient(caster:GetPlayerOwnerID(), "Hero_OgreMagi.Bloodlust.Target")
+	--Sounds:EmitSoundOnClient(caster:GetPlayerOwnerID(), "Hero_OgreMagi.Bloodlust.Target")
 
 	local particleName = "particles/units/heroes/hero_ogre_magi/ogre_magi_bloodlust_cast.vpcf"
 	local particle = ParticleManager:CreateParticle(particleName, PATTACH_ABSORIGIN, caster)
