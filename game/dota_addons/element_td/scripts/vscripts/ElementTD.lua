9dc191742b4f4d131163aff0cf516932dd81361f
MNoya <martinnoya@gmail.com>
2016-02-10 17:20:00 -0300

    Map updated
    - Fog re-enabled

--- game/dota_addons/element_td/scripts/vscripts/ElementTD.lua
+++ game/dota_addons/element_td/scripts/vscripts/ElementTD.lua
@@ -101,7 +101,7 @@ function ElementTD:InitGameMode()
     base_game_mode:SetBuybackEnabled( false )
     --base_game_mode:SetCameraDistanceOverride(1500) -- move the camera higher up
     SendToServerConsole('r_farz 10000')
-    SendToServerConsole('fog_enable 0')
+    --SendToServerConsole('fog_enable 0')
     base_game_mode:SetCustomGameForceHero( "npc_dota_hero_wisp" ) -- Skip hero pick screen
     ------------------------------------------------------
 
