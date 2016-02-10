3b0d3d6f927fab8124166f24c7b92e99cbccdf45
WindStrike72 <windstrike.wcu@gmail.com>
2016-02-10 11:58:31 -0500

    Express Bounties Updated
    Express Mode should now be beatable.

--- game/dota_addons/element_td/scripts/vscripts/gamesettings.lua
+++ game/dota_addons/element_td/scripts/vscripts/gamesettings.lua
@@ -25,7 +25,7 @@ DifficultyObject = createClass({
 function DifficultyObject:GetBountyForWave(wave)
 	local bounty = math.floor(math.pow(self.data.BaseBounty, wave + 4))
 	if EXPRESS_MODE then
-		bounty = math.floor(math.pow(self.data.BaseBountyExpress, wave + 4))
+		bounty = math.floor(math.pow(self.data.BaseBountyExpress, wave + 8))
 	elseif wave == WAVE_COUNT then -- boss wave
 		bounty = 0
 	end
