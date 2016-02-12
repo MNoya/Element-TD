b3bb7051b97b2f330073c9b639e94119d49a351e
MNoya <martinnoya@gmail.com>
2016-02-12 12:17:20 -0300

    Added Haste sounds and particle when hitting max stacks
    #88 Haste sounds reduced

--- game/dota_addons/element_td/scripts/vscripts/towers/triples/HasteTower.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/triples/HasteTower.lua
@@ -19,11 +19,18 @@ function HasteTower:ResetAttackSpeed()
     self.tower:RemoveModifierByName("modifier_wrath")
     self.wrathStacks = 0
     self.tower:SetBaseAttackTime(self.startingBAT)
+    self.soundEnable = true
+    ParticleManager:DestroyParticle(self.particleMax, true)
 end
 
 function HasteTower:OnAttack(keys)
+    self.tower:EmitSound("Haste.Attack")
     if self.wrathStacks < self.wrathCap then
         self.wrathStacks = self.wrathStacks + 1
+    elseif self.soundEnable then
+        self.soundEnable = false
+        self.tower:EmitSound("Haste.Max")
+        self.particleMax = ParticleManager:CreateParticle("particles/units/heroes/hero_troll_warlord/troll_warlord_battletrance_buff.vpcf", PATTACH_ABSORIGIN, self.tower)
     end
 
     self.ability:ApplyDataDrivenModifier(self.tower, self.tower, "modifier_wrath", {})
@@ -51,6 +58,7 @@ function HasteTower:OnCreated()
     self.batStack = GetAbilitySpecialValue("haste_tower_wrath", "bat_decrease_stack")
     self.wrathStacks = 0
     self.timer = nil
+    self.soundEnable = true
     self.startingBAT = self.tower:GetBaseAttackTime()
 end
 
