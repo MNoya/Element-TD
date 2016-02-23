6cefdecdff39cdbc7214f527425c816938e07e02
MNoya <martinnoya@gmail.com>
2016-02-23 08:47:57 -0300

    Resolve #206 - Flame Tower now uses default targeting

--- game/dota_addons/element_td/scripts/vscripts/towers/duals/FlameTower.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/duals/FlameTower.lua
@@ -27,8 +27,6 @@ function FlameTower:OnCreated()
     self.burnDamage = GetAbilitySpecialValue("flame_tower_sunburn", "damage")    
     self.burnAOE = GetAbilitySpecialValue("flame_tower_sunburn", "aoe")    
     self.sunburnDuration = GetAbilitySpecialValue("flame_tower_sunburn", "duration")
-
-    self.tower:AddNewModifier(self.tower, nil, "modifier_attack_targeting", {target_type=TOWER_TARGETING_LOWEST_HP})
 end
 
 function FlameTower:OnAttack(keys)
