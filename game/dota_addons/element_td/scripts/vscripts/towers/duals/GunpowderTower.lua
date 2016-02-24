6bf08571ee65ee573ef555111e9748707342bbef
MNoya <martinnoya@gmail.com>
2016-02-24 15:20:57 -0300

    Added small delay to HoHo-HaHa Sharpnel tower - Resolve #216

--- game/dota_addons/element_td/scripts/vscripts/towers/duals/GunpowderTower.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/duals/GunpowderTower.lua
@@ -23,23 +23,25 @@ function GunpowderTower:OnAttackLanded(keys)
     local particle = ParticleManager:CreateParticle("particles/custom/towers/gunpowder/shrapnel.vpcf", PATTACH_CUSTOMORIGIN, self.tower)    
     ParticleManager:SetParticleControl(particle, 0, origin)
     ParticleManager:SetParticleControl(particle, 1, Vector(self.splashAOE, 1, 1))
-    Timers:CreateTimer(1, function() ParticleManager:DestroyParticle(particle, true) end)
+    Timers:CreateTimer(1.3, function() ParticleManager:DestroyParticle(particle, true) end)
       
     keys.caster:EmitSound("Gunpower.Explosion")
 
     local damage = ApplyAbilityDamageFromModifiers(self.splashDamage[self.tower:GetLevel()], self.tower)    
     DamageEntitiesInArea(target:GetOrigin(), self.splashAOE, self.tower, damage)
 
-    --spawn random explosions around the initial point
+    --spawn random explosions around the initial point, after a small delay
     local rotate_pos = origin + Vector(1,0,0) * 100
-    for i = 1, 4 do          
-        local pos = RotatePosition(origin, QAngle(0, 90*i, 0), rotate_pos)
+    Timers:CreateTimer(0.3, function()
+        for i = 1, 4 do          
+            local pos = RotatePosition(origin, QAngle(0, 90*i, 0), rotate_pos)
 
-        if IsValidEntity(self.tower) then
-            local damage = ApplyAbilityDamageFromModifiers(self.splashDamage[self.tower:GetLevel()], self.tower)    
-            DamageEntitiesInArea(pos, self.splashAOE, self.tower, damage)
+            if IsValidEntity(self.tower) then
+                local damage = ApplyAbilityDamageFromModifiers(self.splashDamage[self.tower:GetLevel()], self.tower)    
+                DamageEntitiesInArea(pos, self.splashAOE, self.tower, damage)
+            end
         end
-    end
+    end)
 end
 
 function GunpowderTower:OnCreated()
