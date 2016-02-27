577230c9b23797c40134b7abc862818709544119
ynohtna92 <a_n_t_h_o_n_y8@hotmail.com>
2016-02-27 14:03:29 +1100

    Scoring is now completely multiplicative. Chaos bounty reduced from 25% to 15% Resolves #235

--- game/dota_addons/element_td/scripts/vscripts/scoring.lua
+++ game/dota_addons/element_td/scripts/vscripts/scoring.lua
@@ -202,7 +202,7 @@ function ScoringObject:GetWaveCleared( wave )
 	local endlessBonus = self:GetEndlessBonus()
 	local leaks = playerData.waveObjects[wave] and playerData.waveObjects[wave].leaks or 0
 	local cleanBonus = self:GetCleanBonus( leaks == 0 )
-	local totalScore = math.ceil(waveClearScore * (1 + cleanBonus + speedBonus + difficultyBonus) * (1 + chaosBonus + endlessBonus))
+	local totalScore = math.ceil(waveClearScore * (1 + cleanBonus) * (1 + speedBonus) * (1 + difficultyBonus) * (1 + chaosBonus) * (1 + endlessBonus))
 
 	return { clearBonus = waveClearScore, cleanBonus = cleanBonus, speedBonus = speedBonus, difficultyBonus = difficultyBonus, chaosBonus = chaosBonus, endlessBonus = endlessBonus, totalScore = totalScore }
 end
@@ -312,7 +312,7 @@ end
 function ScoringObject:GetCreepOrderBonus()
 	local bonus = 0
 	if GameSettings.order == "Chaos" then
-		bonus = 0.25 -- x1.25
+		bonus = 0.15 -- x1.15
 	end
 	return bonus
 end
