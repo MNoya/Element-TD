62f9594e15104525d64e36b87a3f5765b63df17a
MNoya <martinnoya@gmail.com>
2016-02-29 00:21:01 -0300

    Balance was not achieved this day

--- game/dota_addons/element_td/scripts/vscripts/ElementTD.lua
+++ game/dota_addons/element_td/scripts/vscripts/ElementTD.lua
@@ -17,7 +17,7 @@ if not players then
     DEV_MODE = false
     EXPRESS_MODE = false
 
-    VERSION = "B290216"
+    VERSION = "B010316"
 
     START_TIME = GetSystemDate() .. " " .. GetSystemTime()
     END_TIME = nil
