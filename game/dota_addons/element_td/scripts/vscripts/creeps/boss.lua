32a0b9403e9ff5708b4945622e282adfe331c6b7
MNoya <martinnoya@gmail.com>
2016-03-05 18:28:19 -0300

    Added Vengeance to the boss ability sequence (3rd, after heal)

--- game/dota_addons/element_td/scripts/vscripts/creeps/boss.lua
+++ game/dota_addons/element_td/scripts/vscripts/creeps/boss.lua
@@ -17,10 +17,11 @@ CreepBasic)
 CreepBossAbilities = {
         [1]="creep_ability_bulky",
         [2]="creep_ability_heal",
-        [3]="creep_ability_undead",
-        [4]="creep_ability_regen",
-        [5]="creep_ability_fast",
-        [6]="creep_ability_mechanical"
+        [3]="creep_ability_vengeance",
+        [4]="creep_ability_undead",
+        [5]="creep_ability_regen",
+        [6]="creep_ability_fast",
+        [7]="creep_ability_mechanical"
     }
 
 function CreepBoss:OnSpawned()
@@ -84,54 +85,83 @@ function CreepBoss:RegenerateCreepHealth()
     end
 end
 
--- Undead
-function CreepBoss:OnDeath()
+-- Undead and Vengeance
+function CreepBoss:OnDeath(killer)
     local playerData = GetPlayerData(self.creep.playerID)
     local creep = self.creep
     local playerID = creep.playerID
     local creepClass = self.creepClass
 
-    if creep.random_ability ~= "creep_ability_undead" then
-        return
+    if creep.random_ability == "creep_ability_undead" then
+        local newCreep = CreateUnitByName(creepClass, creep:GetAbsOrigin() , false, nil, nil, DOTA_TEAM_NEUTRALS)
+        newCreep.class = creepClass
+        newCreep.playerID = creep.playerID
+        newCreep.waveObject = creep.waveObject
+        newCreep.bounty = creep.bounty
+        newCreep:CreatureLevelUp(newCreep.waveObject.waveNumber-newCreep:GetLevel())
+        
+        creep.waveObject:RegisterCreep(newCreep:entindex())
+        creep.waveObject.creepsRemaining = creep.waveObject.creepsRemaining + 1 -- Increment creep count
+        newCreep:AddNewModifier(newCreep, nil, "modifier_phased", {})
+        newCreep:AddNewModifier(newCreep, nil, "modifier_invulnerable", {})
+        newCreep:AddNewModifier(newCreep, nil, "modifier_invisible_etd", {})
+        newCreep:AddNewModifier(newCreep, nil, "modifier_stunned", {})
+        newCreep:AddNoDraw()
+        newCreep.isUndeadRespawn = true
+
+        newCreep:SetMaxHealth(creep:GetMaxHealth())
+        newCreep:SetBaseMaxHealth(creep:GetMaxHealth())
+        newCreep:SetForwardVector(creep:GetForwardVector())
+        creep.scriptObject = self
+
+        local undead_ability = newCreep:FindAbilityByName("creep_ability_undead")
+        if undead_ability then
+            undead_ability:SetHidden(true)
+        end
+
+        local particle = ParticleManager:CreateParticle("particles/generic_hero_status/death_tombstone.vpcf", PATTACH_ABSORIGIN, creep)
+        ParticleManager:SetParticleControl(particle, 2, Vector(3,0,0))
+
+        -- Respawn Timer
+        Timers:CreateTimer(3, function()
+            ParticleManager:DestroyParticle(particle, true)
+            self:UndeadCreepRespawn()
+        end)
+
+        self.creep = newCreep
+        CREEP_SCRIPT_OBJECTS[newCreep:entindex()] = self
+
+    elseif creep.random_ability == "creep_ability_vengeance" then
+        local ability = self.ability
+        local duration = ability:GetSpecialValueFor("duration")
+        local aoe = ability:GetSpecialValueFor("aoe")
+        local damage_reduction = ability:GetSpecialValueFor("damage_reduction")
+
+        local enemy_towers = FindUnitsInRadius(creep:GetTeamNumber(), killer:GetAbsOrigin(), nil, aoe, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_INVULNERABLE + DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false)
+        for _,tower in pairs(enemy_towers) do       
+            local modifier = tower:FindModifierByName("modifier_vengeance_debuff")
+            if not modifier then
+                ability:ApplyDataDrivenModifier(tower, tower, "modifier_vengeance_debuff", {})
+                modifier = tower:FindModifierByName("modifier_vengeance_debuff")
+            end
+            
+            local stackCount = tower:GetModifierStackCount("modifier_vengeance_debuff", tower) + 1      
+            if modifier then
+                modifier:IncrementStackCount()
+                modifier:SetDuration(duration, true)
+                modifier.baseDamageReduction = damage_reduction
+                modifier.damageReduction = modifier.baseDamageReduction * stackCount
+            end
+
+            ability:ApplyDataDrivenModifier(tower, tower, "modifier_vengeance_multiple", {duration=duration})
+        end
+       
+        local particle = ParticleManager:CreateParticle("particles/custom/creeps/vengeance/death.vpcf", PATTACH_CUSTOMORIGIN, nil)
+        ParticleManager:SetParticleControl(particle, 0, killer:GetAbsOrigin())
+        ParticleManager:SetParticleControl(particle, 1, killer:GetAbsOrigin())
+        ParticleManager:SetParticleControl(particle, 2, Vector(aoe*1.5,1,1))
+        ParticleManager:SetParticleControl(particle, 3, Vector(aoe*2,1,1))
     end
-
-    local newCreep = CreateUnitByName(creepClass, creep:GetAbsOrigin() , false, nil, nil, DOTA_TEAM_NEUTRALS)
-    newCreep.class = creepClass
-    newCreep.playerID = creep.playerID
-    newCreep.waveObject = creep.waveObject
-    newCreep.bounty = creep.bounty
-    newCreep:CreatureLevelUp(newCreep.waveObject.waveNumber-newCreep:GetLevel())
-    
-    creep.waveObject:RegisterCreep(newCreep:entindex())
-    creep.waveObject.creepsRemaining = creep.waveObject.creepsRemaining + 1 -- Increment creep count
-    newCreep:AddNewModifier(newCreep, nil, "modifier_phased", {})
-    newCreep:AddNewModifier(newCreep, nil, "modifier_invulnerable", {})
-    newCreep:AddNewModifier(newCreep, nil, "modifier_invisible_etd", {})
-    newCreep:AddNewModifier(newCreep, nil, "modifier_stunned", {})
-    newCreep:AddNoDraw()
-    newCreep.isUndeadRespawn = true
-
-    newCreep:SetMaxHealth(creep:GetMaxHealth())
-    newCreep:SetBaseMaxHealth(creep:GetMaxHealth())
-    newCreep:SetForwardVector(creep:GetForwardVector())
-    creep.scriptObject = self
-
-    local undead_ability = newCreep:FindAbilityByName("creep_ability_undead")
-    if undead_ability then
-        undead_ability:SetHidden(true)
-    end
-
-    local particle = ParticleManager:CreateParticle("particles/generic_hero_status/death_tombstone.vpcf", PATTACH_ABSORIGIN, creep)
-    ParticleManager:SetParticleControl(particle, 2, Vector(3,0,0))
-
-    -- Respawn Timer
-    Timers:CreateTimer(3, function()
-        ParticleManager:DestroyParticle(particle, true)
-        self:UndeadCreepRespawn()
-    end)
-
-    self.creep = newCreep
-    CREEP_SCRIPT_OBJECTS[newCreep:entindex()] = self
 end
 
 -- Undead
@@ -163,63 +193,6 @@ function CreepBoss:UndeadCreepRespawn()
     local h = ParticleManager:CreateParticle("particles/units/heroes/hero_skeletonking/skeletonking_reincarnation.vpcf", PATTACH_CUSTOMORIGIN, creep) --play a cool particle effect :D
     ParticleManager:SetParticleControl(h, 0, creep:GetAbsOrigin())
     ParticleManager:SetParticleControlEnt(h, 0, creep, PATTACH_POINT_FOLLOW, "attach_hitloc", creep:GetOrigin(), true)
-end
-
--- Swarm
-function CreepBoss:OnTakeDamage(keys)   
-    if self.creep:GetHealth() > 0 and self.creep:GetHealthPercent() <= 50 and not self.creep.isSwarm then
-    
-        if self.creep.random_ability ~= "creep_ability_swarm" then
-            return
-        end
-
-        local swarm = SpawnEntity(self.creep:GetUnitName(), self.creep.playerID, self.creep:GetOrigin())
-        swarm.class = creepClass
-        swarm.playerID = self.creep.playerID
-        swarm.waveObject = self.creep.waveObject
-        self.creep.waveObject:RegisterCreep(swarm:entindex())
-        self.creep.waveObject.creepsRemaining = self.creep.waveObject.creepsRemaining + 1 -- Increment creep count
-        swarm.isSwarm = true
-        local newMaxHealth = self.creep:GetMaxHealth()/2
-        swarm:SetMaxHealth(newMaxHealth)
-        swarm:SetBaseMaxHealth(newMaxHealth)
-        swarm:SetHealth(newMaxHealth)
-        swarm:SetDeathXP(0)
-        swarm:SetForwardVector(self.creep:GetForwardVector())
-
-        swarm.scriptObject:OnSpawned()        
-
-        self.creep:SetMaxHealth(newMaxHealth)
-        self.creep:SetBaseMaxHealth(newMaxHealth)
-        self.creep:SetHealth(newMaxHealth)
-
-        local newScale = self.creep:GetModelScale()*0.8
-
-        self.creep:SetModelScale(newScale)
-        swarm:SetModelScale(newScale)
-        self.creep.isSwarm = true
-
-        local playerID = self.creep.playerID
-        local wave = self.creep.waveObject:GetWaveNumber()
-
-        -- split the bounty between the two units even if it is an odd amount
-        local bounty1 = GetPlayerDifficulty(playerID):GetBountyForWave(wave)
-        local bounty2
-        if bounty1 % 2 == 0 then
-            bounty1 = bounty1 / 2
-            bounty2 = bounty1
-        else
-            bounty1 = math.floor(bounty1 / 2)
-            bounty2 = bounty1 + 1
-        end
-
-        self.creep.bounty = bounty1
-
-        swarm.bounty = bounty2
-
-        local playerData = GetPlayerData(playerID)
-        CreateMoveTimerForCreep(swarm, playerData.sector + 1)
-    end
 end
 
 -- Heal
