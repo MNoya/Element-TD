1900aa3dd44855bc76ec5678c174f6340cfa6c2a
MNoya <martinnoya@gmail.com>
2016-03-09 11:40:58 -0300

    Quark Tower stacks are now halved when changing targets (rounding down) - Resolve #293

--- game/dota_addons/element_td/scripts/vscripts/ElementTD.lua
+++ game/dota_addons/element_td/scripts/vscripts/ElementTD.lua
@@ -17,7 +17,7 @@ if not players then
     DEV_MODE = false
     EXPRESS_MODE = false
 
-    VERSION = "1.01"
+    VERSION = "1.1"
 
     START_TIME = GetSystemDate() .. " " .. GetSystemTime()
     END_TIME = nil
