fe37c0c53ecbb638a803d79242f16ec735758752
MNoya <martinnoya@gmail.com>
2016-03-17 14:25:06 -0300

    New Version 1.2 today

--- game/dota_addons/element_td/scripts/vscripts/ElementTD.lua
+++ game/dota_addons/element_td/scripts/vscripts/ElementTD.lua
@@ -17,7 +17,7 @@ if not players then
     DEV_MODE = false
     EXPRESS_MODE = false
 
-    VERSION = "1.1"
+    VERSION = "1.2"
 
     START_TIME = GetSystemDate() .. " " .. GetSystemTime()
     END_TIME = nil
