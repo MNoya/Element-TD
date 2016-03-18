e590c5d4363aa0639f765294170823ea00194e57
Quintinity <vlad.marica1@gmail.com>
2016-03-17 21:59:42 -0300

    Maybe interest on reconnect will actually work now

--- game/dota_addons/element_td/scripts/vscripts/interest.lua
+++ game/dota_addons/element_td/scripts/vscripts/interest.lua
@@ -36,7 +36,7 @@ function InterestManager:HandlePlayerReconnect(playerID)
 			local timerName = InterestManager.timers[playerID]
 			if InterestManager.timers[playerID] and Timers.timers[timerName] then
 				local timeRemaining = Timers.timers[timerName].endTime - GameRules:GetGameTime()
-				CustomGameEventManager:Send_ServerToPlayer(player, "etd_resume_interest", { timeRemaining = interestData.TimeRemaining })
+				CustomGameEventManager:Send_ServerToPlayer(player, "etd_resume_interest", { timeRemaining = timeRemaining })
 			end
 		end
 
