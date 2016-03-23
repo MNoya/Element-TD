b4ec5864a1629176fcac36ab6704a1628b2fc307
MNoya <martinnoya@gmail.com>
2016-03-23 15:19:32 -0300

    Fixed #315 Ephemeral Tower not keeping its damage reduction on upgrade/cancel

--- game/dota_addons/element_td/scripts/vscripts/towers/triples/EphemeralTower.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/triples/EphemeralTower.lua
@@ -21,6 +21,10 @@ function EphemeralTower:ResetDamage(keys)
         Timers:RemoveTimer(self.timer)
     end
 
+    if self.tower.upgraded then
+        return
+    end
+
     self.current_stacks = 0
 
     self.tower:RemoveModifierByName("modifier_reset_damage")
@@ -57,7 +61,7 @@ function EphemeralTower:OnAttack(keys)
 
                     if self.current_stacks < self.maxStacks then                        
                         local phasing_stack = self.tower:FindModifierByName("modifier_phasing_stack")
-                        if phasing_stack == nil then
+                        if not phasing_stack then
                             self.ability:ApplyDataDrivenModifier(self.tower, self.tower, "modifier_phasing_stack", {})
                         end
                         self.current_stacks = self.current_stacks + 1
@@ -80,6 +84,23 @@ function EphemeralTower:OnAttackLanded(keys)
     local target = keys.target
     local damage = self.tower:GetAverageTrueAttackDamage()
     DamageEntity(target, self.tower, damage)
+end
+
+function EphemeralTower:ApplyUpgradeData(data)
+    if data.current_stacks then
+        self.current_stacks = data.current_stacks
+        local phasing_stack = self.tower:FindModifierByName("modifier_phasing_stack")
+        if not phasing_stack then
+            self.ability:ApplyDataDrivenModifier(self.tower, self.tower, "modifier_phasing_stack", {})
+        end
+        self.tower:SetModifierStackCount("modifier_phasing_stack", self.tower, self.current_stacks)
+    end
+end
+
+function EphemeralTower:GetUpgradeData()
+    return {
+        current_stacks = self.current_stacks
+    }
 end
 
 function EphemeralTower:OnCreated()
