a0d794bf74bdcc411a0a073bff4f9f0d8eefe871
MNoya <martinnoya@gmail.com>
2016-03-26 19:15:52 -0300

    Some possible fixes for undead and end game for player crashes #324
    Added -debug_abilities command

--- game/dota_addons/element_td/scripts/vscripts/mechanics/abilities.lua
+++ game/dota_addons/element_td/scripts/vscripts/mechanics/abilities.lua
@@ -10,3 +10,22 @@ function CDOTABaseAbility:ToggleOff()
     end
 end
 
+function DebugMainSelectedAbilities( playerID )
+    local ent = PlayerResource:GetMainSelectedEntity(playerID)
+    local unit = EntIndexToHScript(ent)
+    if unit then
+        PrintAbilities(unit)
+    end
+end
+
+function PrintAbilities( unit )
+    print("List of Abilities in "..unit:GetUnitName())
+    for i=0,16 do
+        local ability = unit:GetAbilityByIndex(i)
+        if ability then
+            local output = i.." - "..ability:GetAbilityName()
+            if ability:IsHidden() then output = output.." (Hidden)" end
+            print(output)
+        end
+    end
+end
