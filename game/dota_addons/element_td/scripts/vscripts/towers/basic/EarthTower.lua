6ca5da61f450451c17b62ef525913402888820b2
WindStrike72 <windstrike.wcu@gmail.com>
2016-05-27 22:25:13 -0400

    Random 1.8.1 Half-Tested Shenanigans
    - Brought to you by the "We didn't balance this at all" foundation. No
    really, this is just some preliminary stuff, patch is still half a week
    off at least.

--- game/dota_addons/element_td/scripts/vscripts/towers/basic/EarthTower.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/basic/EarthTower.lua
@@ -21,7 +21,6 @@ function EarthTower:OnAttackLanded(keys)
     DamageEntitiesInArea(location, self.halfAOE, self.tower, damage / 2);
     DamageEntitiesInArea(location, self.fullAOE, self.tower, damage / 2);
 
-    --[[Removed in 1.8
     Timers:CreateTimer(self.aftershockDelay, function()
         local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_tiny/tiny_death_rocks_b.vpcf", PATTACH_CUSTOMORIGIN, nil)
         ParticleManager:SetParticleControl(particle, 0, location)
@@ -29,7 +28,7 @@ function EarthTower:OnAttackLanded(keys)
         local aftershockDamage = damage * self.aftershockDamage
         DamageEntitiesInArea(location, self.halfAOE, self.tower, aftershockDamage / 2);
         DamageEntitiesInArea(location, self.fullAOE, self.tower, aftershockDamage / 2);
-    end)]]
+    end)
 end
 
 --[[Old stun prevention
@@ -43,9 +42,9 @@ end
 function EarthTower:OnCreated()
     self.fullAOE =  tonumber(GetUnitKeyValue(self.towerClass, "AOE_Full"));
     self.halfAOE =  tonumber(GetUnitKeyValue(self.towerClass, "AOE_Half"));
-    --[[self.ability = AddAbility(self.tower, "earth_tower_aftershock")
+    self.ability = AddAbility(self.tower, "earth_tower_aftershock")
     self.aftershockDamage = self.ability:GetSpecialValueFor("aftershock_damage_pct") * 0.01
-    self.aftershockDelay = self.ability:GetSpecialValueFor("aftershock_delay")]]
+    self.aftershockDelay = self.ability:GetSpecialValueFor("aftershock_delay")
 
     --self.ministun_duration = self.ability:GetSpecialValueFor("ministun_duration")
     --self.threshold_duration = 0.9
