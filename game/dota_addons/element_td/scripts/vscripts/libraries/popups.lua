00b2bc4186d0703502a214dabfd27ef7b54a4cb4
Quintinity <vlad.marica1@gmail.com>
2016-09-13 15:01:32 -0300

    #453 - Fixed tower selling particles

--- game/dota_addons/element_td/scripts/vscripts/libraries/popups.lua
+++ game/dota_addons/element_td/scripts/vscripts/libraries/popups.lua
@@ -167,13 +167,13 @@ function PopupLegion(target, number)
     ParticleManager:SetParticleControl( particle, 3, Vector(targetPos.x, targetPos.y, targetPos.z+322) )
 end
 
-function PopupAlchemistGold(target, number)
+function PopupAlchemistGold(target, number, player)
     local symbol = 0 -- "+" presymbol
     local color = Vector(255, 200, 33) -- Gold
     local lifetime = 3
     local digits = string.len(number) + 1
     local particleName = "particles/units/heroes/hero_alchemist/alchemist_lasthit_msg_gold.vpcf"
-    local particle = ParticleManager:CreateParticleForPlayer( particleName, PATTACH_CUSTOMORIGIN, target, target:GetPlayerOwner() )
+    local particle = ParticleManager:CreateParticleForPlayer(particleName, PATTACH_CUSTOMORIGIN, target, player or target:GetPlayerOwner())
     ParticleManager:SetParticleControl(particle, 0, target:GetAbsOrigin())
     ParticleManager:SetParticleControl(particle, 1, Vector(symbol, number, symbol))
     ParticleManager:SetParticleControl(particle, 2, Vector(lifetime, digits, 0))
