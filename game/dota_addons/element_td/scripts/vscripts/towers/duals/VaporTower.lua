0fbf0666f6c260277be0bd5c4962740d0530eb9d
Quintinity <vlad.marica1@gmail.com>
2016-09-13 14:18:30 -0300

    #447 - Vapor Tower update

--- game/dota_addons/element_td/scripts/vscripts/towers/duals/VaporTower.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/duals/VaporTower.lua
@@ -1,6 +1,6 @@
 -- Vapor Tower class (Fire + Water)
--- Non-targeting tower whose attack does 30/150/750 damage to creeps in 700 area around the tower. 
--- Then 0.5 seconds later it does 15/75/375 damage in 350 area around each creep hit.
+-- Non-targeting tower whose attack does 30/150/750 damage to creeps in 700 area around the tower.
+-- The damage dealt increases the more creeps are in the AoE
 
 VaporTower = createClass({
         tower = nil,
@@ -19,7 +19,8 @@ nil)
 function VaporTower:VaporWaveAttack()
     local creeps = GetCreepsInArea(self.tower:GetOrigin(), self.initialAOE)    
     local initial_damage = ApplyAbilityDamageFromModifiers(self.initialDamage[self.level], self.tower)    
-    local aftershock_damage = ApplyAbilityDamageFromModifiers(self.aftershockDamage[self.level], self.tower)    
+    local damage_per_creep = ApplyAbilityDamageFromModifiers(self.damagePerCreep[self.level], self.tower)    
+    local final_damage = initial_damage + (#creeps * damage_per_creep)
 
     if self.tower:GetHealthPercent() == 100 and #creeps > 0 then
         self.tower:StartGesture(ACT_DOTA_CAST_ABILITY_2) --crush
@@ -31,16 +32,7 @@ function VaporTower:VaporWaveAttack()
             local particle = ParticleManager:CreateParticle("particles/custom/towers/vapor/tako.vpcf", PATTACH_ABSORIGIN, creep)     
             ParticleManager:SetParticleControl(particle, 1, Vector(self.initialAOE/4, 1, 1)) 
 
-            DamageEntity(creep, self.tower, initial_damage)    
-
-            Timers:CreateTimer(0.5, function()
-                local aftershock_creeps = GetCreepsInArea(creep:GetAbsOrigin(), self.aftershockAOE)
-                for __, creep2 in pairs(aftershock_creeps) do
-                    if creep2:entindex() ~= creep:entindex() and creep2:IsAlive() then --don't damage self with aftershock
-                        DamageEntity(creep2, self.tower, aftershock_damage)    
-                    end
-                end
-            end)
+            DamageEntity(creep, self.tower, final_damage)    
         end
     end
 end
@@ -50,10 +42,9 @@ function VaporTower:OnCreated()
     local spellName = "vapor_tower_evaporate"
     self.ability = AddAbility(self.tower, spellName, self.level)        
 
-    self.initialDamage = GetAbilitySpecialValue(spellName, "damage")    
-    self.aftershockDamage = GetAbilitySpecialValue(spellName, "aftershock_damage")    
-    self.initialAOE = GetAbilitySpecialValue(spellName, "aoe")+self.tower:GetHullRadius()
-    self.aftershockAOE = GetAbilitySpecialValue(spellName, "aftershock_aoe")
+    self.initialDamage = GetAbilitySpecialValue(spellName, "base_damage")    
+    self.damagePerCreep = GetAbilitySpecialValue(spellName, "damage_per_creep")    
+    self.initialAOE = GetAbilitySpecialValue(spellName, "aoe") + self.tower:GetHullRadius()
     self.playerID = self.tower:GetPlayerOwnerID()
 
     local time = 1 / self.tower:GetAttacksPerSecond()
