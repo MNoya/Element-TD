84320b7f543d2db8edd868c8d9ab0779159a6a8c
WindStrike72 <windstrike.wcu@gmail.com>
2016-10-30 03:14:10 -0400

    Gave Moss its AoE back, bumped its max multiplier to x2

--- game/dota_addons/element_td/scripts/vscripts/towers/duals/MossTower.lua
+++ game/dota_addons/element_td/scripts/vscripts/towers/duals/MossTower.lua
@@ -19,19 +19,35 @@ nil)
 
 function MossTower:OnAttackLanded(keys)
     local target = keys.target    
-    local damage = self.tower:GetAverageTrueAttackDamage(target) * (1 + (0.5 * (target:GetHealth() / target:GetMaxHealth())))
-	DamageEntity(target, self.tower, damage);
+    local damage = self.tower:GetAverageTrueAttackDamage(target)
 
-    local popupDamage = damage * (1 + (0.5 * (target:GetHealth() / target:GetMaxHealth())))
+    local popupDamage = damage * (1 + (target:GetHealth() / target:GetMaxHealth()))
     if target:IsAlive() then
         popupDamage = ApplyElementalDamageModifier(popupDamage, GetDamageType(self.tower), GetArmorType(target))
         PopupGreenCriticalDamage(self.tower, math.floor(popupDamage))
+    end
+
+    local creepsInHalfAOE = GetCreepsInArea(target:GetOrigin(), self.halfAOE)    
+    local creepsInFullAOE = GetCreepsInArea(target:GetOrigin(), self.fullAOE)    
+    
+    local damages = {}     -- because slash damage is dealt in two instances, we need to calculate the correct damage beforehand
+    for _,v in pairs(creepsInHalfAOE) do
+        damages[v:entindex()] = damage * (1 + (v:GetHealth() / v:GetMaxHealth()))
+    end
+
+    for _,v in pairs(creepsInHalfAOE) do
+        DamageEntity(v, self.tower, damages[v:entindex()] / 2)    
+    end
+    for _,v in pairs(creepsInFullAOE) do
+        DamageEntity(v, self.tower, damages[v:entindex()] / 2)    
     end
 
 end
 
 function MossTower:OnCreated()
     AddAbility(self.tower, "moss_tower_spore")
+	self.fullAOE =  tonumber(GetUnitKeyValue(self.towerClass, "AOE_Full"));
+	self.halfAOE =  tonumber(GetUnitKeyValue(self.towerClass, "AOE_Half"));
     self.tower:AddNewModifier(self.tower, nil, "modifier_attack_targeting", {target_type=TOWER_TARGETING_HIGHEST_HP, keep_target=true}) 
 end
 
