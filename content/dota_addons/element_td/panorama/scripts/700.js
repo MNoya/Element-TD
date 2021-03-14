28998b599e60912c8f7a05f60a37a523dc4c2657
ynohtna92 <a_n_t_h_o_n_y8@hotmail.com>
2021-03-14 18:27:39 +1100

    Remove dota 2 hud elements

--- content/dota_addons/element_td/panorama/scripts/700.js
+++ content/dota_addons/element_td/panorama/scripts/700.js
@@ -6,6 +6,10 @@
 	var newPanel = $.CreatePanel("Panel", hud, "SimpleScoreboard");
 	newPanel.BLoadLayout( "file://{resources}/layout/custom_game/simple_scoreboard.xml", false, false );
 
+	// Hide Topbar background
+	var topbar = hud.FindChildTraverse("HUDElements").FindChildTraverse("topbar").FindChildTraverse("HUDSkinTopBarBG");
+	topbar.style.visibility = "collapse";
+
 	// Remove talent tree and backpack
 	var newUI = hud.FindChildTraverse("HUDElements").FindChildTraverse("lower_hud").FindChildTraverse("center_with_stats").FindChildTraverse("center_block");
 	//try the neat way to remove the tree itself
@@ -20,9 +24,13 @@
 	//fuck backpack UI (We have Lua filling these slots with junk, and if the player can't touch them it should be effectively disabled)
 	newUI.FindChildTraverse("inventory").FindChildTraverse("inventory_items").FindChildTraverse("inventory_backpack_list").style.visibility = "collapse";
 
+	// Hide Secondary Inventory
+	newUI.FindChildTraverse("right_flare").style.visibility = "collapse";
+	newUI.FindChildTraverse("inventory_composition_layer_container").style.visibility = "collapse";
+
 	// Remove Scan and Glyph
 	var glyphScanContainer = hud.FindChildTraverse("HUDElements").FindChildTraverse("minimap_container").FindChildTraverse("GlyphScanContainer");
-	glyphScanContainer.FindChildTraverse("RadarButton").style.visibility = "collapse";
+	glyphScanContainer.style.visibility = "collapse";
 
 	// Fix side info panel
 	var gameinfo = hud.FindChildTraverse("CustomUIRoot").FindChildTraverse("CustomUIContainer_GameInfo");
